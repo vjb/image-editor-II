1e2b57b5df83206ed6eaf406ce5e83f0b9fcc1d4
vjb <vjb@users.noreply.github.com>
2018-08-12 10:29:44 -0400

    added Matt's HTML, CSS improvements ... now integrating the JS

--- src/ImageEditor/widget/ImageEditor.js
+++ src/ImageEditor/widget/ImageEditor.js
@@ -20,7 +20,7 @@ define([
 
 
     "dojo/text!ImageEditor/widget/template/ImageEditor.html"
-], function(declare, _WidgetBase, _TemplatedMixin, dom, dojoDom, dojoProp, dojoGeometry, dojoClass, dojoStyle, dojoConstruct, dojoArray, lang, dojoText, dojoHtml, dojoEvent, fabric, canvasToBlob, widgetTemplate) {
+], function (declare, _WidgetBase, _TemplatedMixin, dom, dojoDom, dojoProp, dojoGeometry, dojoClass, dojoStyle, dojoConstruct, dojoArray, lang, dojoText, dojoHtml, dojoEvent, fabric, canvasToBlob, widgetTemplate) {
     "use strict";
 
     return declare("ImageEditor.widget.ImageEditor", [_WidgetBase, _TemplatedMixin], {
@@ -51,16 +51,16 @@ define([
         _handles: null,
         _contextObj: null,
 
-        constructor: function() {
+        constructor: function () {
             this._handles = [];
         },
 
-        postCreate: function() {
+        postCreate: function () {
             logger.debug(this.id + ".postCreate");
 
             this.canvas = new fabric.Canvas(this.canvasNode);
 
-           
+
 
             // fabric.Image.fromURL('/img/MyFirstModule$_16_base_back.png', function (oImg) {
             //     this.canvas.add(oImg);
@@ -68,7 +68,7 @@ define([
             this._setupEvents();
         },
 
-        update: function(obj, callback) {
+        update: function (obj, callback) {
             logger.debug(this.id + ".update");
 
             this._contextObj = obj;
@@ -78,17 +78,17 @@ define([
             this._updateRendering(callback);
         },
 
-        resize: function(box) {
+        resize: function (box) {
             logger.debug(this.id + ".resize");
             // this._resizeCanvas(); // could resize to window?
             this._updateRendering();
         },
 
-        uninitialize: function() {
+        uninitialize: function () {
             logger.debug(this.id + ".uninitialize");
         },
 
-        _updateRendering: function(callback) {
+        _updateRendering: function (callback) {
             logger.debug(this.id + "._updateRendering");
             this._resizeCanvas();
             this._drawCanvasBackground();
@@ -96,7 +96,7 @@ define([
         },
 
         // Shorthand for running a microflow
-        _execMf: function(mf, guid, cb) {
+        _execMf: function (mf, guid, cb) {
             logger.debug(this.id + "._execMf");
             if (mf && guid) {
                 mx.ui.action(mf, {
@@ -104,12 +104,12 @@ define([
                         applyto: "selection",
                         guids: [guid]
                     },
-                    callback: lang.hitch(this, function(objs) {
+                    callback: lang.hitch(this, function (objs) {
                         if (cb && typeof cb === "function") {
                             cb(objs);
                         }
                     }),
-                    error: function(error) {
+                    error: function (error) {
                         console.debug(error.description);
                     }
                 }, this);
@@ -117,7 +117,7 @@ define([
         },
 
         // Shorthand for executing a callback, adds logging to your inspector
-        _executeCallback: function(cb, from) {
+        _executeCallback: function (cb, from) {
             logger.debug(this.id + "._executeCallback" + (from ? " from " + from : ""));
             if (cb && typeof cb === "function") {
                 cb();
@@ -127,7 +127,7 @@ define([
         /**
          * resize the canvas when the window changes size
         //  */
-        _resizeCanvas: function() {
+        _resizeCanvas: function () {
             this.canvasNode.width = this.canvasWidth;
             this.canvasNode.height = this.canvasHeight;
             this.canvas.setWidth(this.canvasWidth);
@@ -135,7 +135,7 @@ define([
             this.canvas.calcOffset();
         },
 
-        _plotImageFromContext: function() {
+        _plotImageFromContext: function () {
 
         },
 
@@ -143,9 +143,9 @@ define([
          * REQUIRES CONTEXT
          * - resizes the canvas
          */
-        _drawCanvasBackground: function() {
+        _drawCanvasBackground: function () {
             var imgUrl = mx.data.getDocumentUrl(this._contextObj.getGuid(), this._contextObj.get("changedDate"), false);
-            fabric.Image.fromURL(imgUrl, function(img) {
+            fabric.Image.fromURL(imgUrl, function (img) {
                 var imgWidth = img.width,
                     imgHeight = img.height,
                     aspectRatio = imgHeight / imgWidth,
@@ -166,7 +166,7 @@ define([
             }.bind(this));
         },
 
-        _setupEvents: function() {
+        _setupEvents: function () {
             this.connect(this.addTextButtonNode, "click", this._drawInteractiveText);
             this.connect(this.addArrowButtonNode, "click", this._drawArrow);
             this.connect(this.saveButtonNode, "click", this._saveToNewImage);
@@ -176,26 +176,9 @@ define([
             this.connect(this.textFontSizeNode, "change", this._changeFontSize);
             this.connect(this.textColorNode, "change", this._changeColor);
             this.connect(this.fontFamilyNode, "change", this._changeFontFamily);
-           // this.connect(this.canvasNode, 'object:scaling', function(){console.log("scalingh")} )
-
-           this.connect(this.canvasNode, 'object:rotating', function(){
-            var obj = this.canvas.getActiveObject();
-            obj.set({
-                opacity: 0.5
-            });
-            this.renderAll();
-        }),
 
 
-        /*   this.canvas.on('object:rotating', function(){
-            var obj = this.getActiveObject();
-            obj.set({
-                opacity: 0.5
-            });
-            this.renderAll();
-        });*/
-
-            this.canvas.on('object:scaling', function(){
+            this.canvas.on('object:rotating', function () {
                 var obj = this.getActiveObject();
                 obj.set({
                     opacity: 0.5
@@ -203,17 +186,25 @@ define([
                 this.renderAll();
             });
 
-            this.canvas.on('object:moving', function(){
+            this.canvas.on('object:scaling', function () {
                 var obj = this.getActiveObject();
                 obj.set({
                     opacity: 0.5
                 });
                 this.renderAll();
             });
-            
-     
 
-            this.canvas.on('mouse:up', function(){
+            this.canvas.on('object:moving', function () {
+                var obj = this.getActiveObject();
+                obj.set({
+                    opacity: 0.5
+                });
+                this.renderAll();
+            });
+
+
+
+            this.canvas.on('mouse:up', function () {
                 var obj = this.getActiveObject();
                 obj.set({
                     opacity: 1.0
@@ -221,11 +212,11 @@ define([
                 this.renderAll();
             });
 
-           
+
         },
 
 
-        _changeFontFamily: function() {
+        _changeFontFamily: function () {
 
             this.canvas.getActiveObject().setFontFamily(this.fontFamilyNode.value);
 
@@ -233,20 +224,20 @@ define([
 
         },
 
-        _changeColor: function() {
+        _changeColor: function () {
             this.canvas.getActiveObject().setFill(this.textColorNode.value);
             this.canvas.getActiveObject().setStroke(this.textColorNode.value);
             this.canvas.renderAll();
 
         },
 
-        _changeFontSize: function() {
+        _changeFontSize: function () {
             this.canvas.getActiveObject().setFontSize(this.textFontSizeNode.value);
             this.canvas.renderAll();
 
         },
 
-        _makeBlue: function() {
+        _makeBlue: function () {
             var activeObject = this.canvas.getActiveObject()
 
             this.canvas.getActiveObject().set("fill", '#0000ff');
@@ -260,7 +251,7 @@ define([
 
         },
 
-        _increaseFont: function() {
+        _increaseFont: function () {
             var activeObject = this.canvas.getActiveObject()
             var increment = "5";
             var currFontSize = activeObject.get("fontSize");
@@ -272,7 +263,7 @@ define([
         },
 
 
-        _deleteObject: function() {
+        _deleteObject: function () {
             var activeObject = this.canvas.getActiveObject()
 
 
@@ -282,7 +273,7 @@ define([
 
         },
 
-        _drawInteractiveText: function() {
+        _drawInteractiveText: function () {
             var itext = new fabric.IText('Enter your Text', {
                 left: this.canvas.getWidth() / 2,
                 top: this.canvas.getHeight() / 2,
@@ -311,7 +302,7 @@ define([
             this.canvas.add(itext);
         },
 
-        _drawArrow: function() {
+        _drawArrow: function () {
             var triangle = new fabric.Triangle({
                 width: 10,
                 height: 15,
@@ -369,14 +360,14 @@ define([
          *  - save the canvas contents to a new image object
          *  - copy the association from the source image->parent to this new object
          */
-        _saveToNewImage: function() {
+        _saveToNewImage: function () {
             this.saveButtonNode.setAttribute("disabled", null);
             this.saveButtonNode.innerText = "Saving..."
             this._getNewImageObject()
                 .then(this._copyParentAssociationToNewObject.bind(this))
                 .then(this._saveCanvasContentsToImage.bind(this))
                 .then(this._executeCompletedMicroflow.bind(this))
-                .then(function() {
+                .then(function () {
                     this.saveButtonNode.removeAttribute("disabled");
                     this.saveButtonNode.innerText = "💾";
                 }.bind(this));
@@ -391,16 +382,16 @@ define([
          * @since Jul 30, 2018
          * @returns newly created MyImage object
          */
-        _getNewImageObject: function() {
-            return new Promise(lang.hitch(this, function(resolve, reject) {
+        _getNewImageObject: function () {
+            return new Promise(lang.hitch(this, function (resolve, reject) {
                 // create a new object of this entity
                 mx.data.create({
                     entity: this._contextObj.getEntity(),
-                    callback: lang.hitch(this, function(obj) {
+                    callback: lang.hitch(this, function (obj) {
                         console.log("The object has been created");
                         resolve(obj);
                     }),
-                    error: function(e) {
+                    error: function (e) {
                         reject("there was an error creating this object");
                     },
                 });
@@ -410,8 +401,8 @@ define([
         /**
          * @returns MyImage Object with parent association set and the IsAnnotated flag set
          */
-        _copyParentAssociationToNewObject: function(object) {
-            return new Promise(lang.hitch(this, function(resolve, reject) {
+        _copyParentAssociationToNewObject: function (object) {
+            return new Promise(lang.hitch(this, function (resolve, reject) {
                 var associationName = this.pathToParent.split("/")[0];
                 // context object exists and is tied to a parent entity
 
@@ -426,12 +417,12 @@ define([
         /**
          * @return guid of newly saved object
          */
-        _saveCanvasContentsToImage: function(object) {
-            return new Promise(lang.hitch(this, function(resolve, reject) {
+        _saveCanvasContentsToImage: function (object) {
+            return new Promise(lang.hitch(this, function (resolve, reject) {
                 this.canvas.deactivateAll().renderAll();
                 //msToBlob()
                 //toBlob
-                this.canvasNode.toBlob(lang.hitch(this, function(blob) {
+                this.canvasNode.toBlob(lang.hitch(this, function (blob) {
                     var fname = "img_" + new Date().toISOString().replace(/\W/g, "") + ".jpg";
                     window.mx.data.saveDocument(
                         object.getGuid(), fname, {
@@ -439,11 +430,11 @@ define([
                             height: 480
                         },
                         blob,
-                        lang.hitch(this, function() {
+                        lang.hitch(this, function () {
                             console.log("ok");
                             resolve(object.getGuid());
                         }),
-                        function(err) {
+                        function (err) {
                             reject("error");
                         });
                 }));
@@ -454,12 +445,12 @@ define([
         /**
          * Look at the context object, and draw the right image (based on the mapping in this.imageMapping)
          */
-        _drawDefaultCMB: function() {
+        _drawDefaultCMB: function () {
             var key = this._contextObj.get(this.imAttribute); // "_16Back"
-            var image = this.imageMapping.find(function(pair) {
+            var image = this.imageMapping.find(function (pair) {
                 return pair.imKey === key
             }); // {imKey: "_16Back", imImage: "...?"}
-            fabric.Image.fromURL(image.imImage, function(oImg) {
+            fabric.Image.fromURL(image.imImage, function (oImg) {
                 oImg.set({
                     width: 150,
                     height: 150,
@@ -485,8 +476,8 @@ define([
         /**
          * execute the specified Microflow, if one exists
          */
-        _executeCompletedMicroflow: function(guid) {
-            return new Promise(lang.hitch(this, function(resolve, reject) {
+        _executeCompletedMicroflow: function (guid) {
+            return new Promise(lang.hitch(this, function (resolve, reject) {
                 if (this.onUploadComplete) {
                     mx.data.action({
                         params: {
